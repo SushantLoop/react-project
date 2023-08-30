2987ea3da02a65d143adfefae6cec994f49d46fc
SushantLoop <sushant@loopkitchen.com>
2023-08-30 15:04:29 +0530

    Ignore

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-
       <header className="App-header">
-        
         <h1>ESLint with Husky</h1>
       </header>
     </div>

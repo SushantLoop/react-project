da249bcd0cfa317419a02ada5a48413145193607
SushantLoop <sushant@loopkitchen.com>
2023-08-31 10:59:57 +0530

    forgot build

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Button = () => {
-  return <button>Button</button>
+  return (
+    <div>
+      <button>Button</button>
+    </div>
+  )
 }
 
 export default Button
